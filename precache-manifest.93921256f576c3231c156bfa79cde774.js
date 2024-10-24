e9d19f195de5b523d42adfa1b239a55b609d2333
Ciana12 <119511355+Ciana12@users.noreply.github.com>
2024-10-24 11:59:42 +0800

    Updates

---
+++ precache-manifest.93921256f576c3231c156bfa79cde774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc8b672e64dfd6378a2d201724b98b1",
+    "url": "/scanner.github.io/index.html"
+  },
+  {
+    "revision": "b38460798e89d2a6aed8",
+    "url": "/scanner.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ed20564130d2bc09995",
+    "url": "/scanner.github.io/static/js/2.7d5375b3.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/scanner.github.io/static/js/2.7d5375b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38460798e89d2a6aed8",
+    "url": "/scanner.github.io/static/js/main.bd72b66b.chunk.js"
+  },
+  {
+    "revision": "bbefe545b3418ab44f74",
+    "url": "/scanner.github.io/static/js/runtime-main.9847cca3.js"
+  }
+]);
